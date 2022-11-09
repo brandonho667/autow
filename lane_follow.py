8cae202ab46986be5a027c82631ffc19b34fa635
Brandon <b1ho@ucsd.edu>
2022-11-08 21:07:03 -0800

    added lane detection

--- lane_follow.py
+++ lane_follow.py
@@ -3,37 +3,70 @@ import cv2
 import numpy as np
 from utils.lane_detect import *
 import depthai as dai
+import signal
 
-# Create pipeline
-pipeline = dai.Pipeline()
-cam = pipeline.create(dai.node.ColorCamera)
-cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
-cam.setPreviewSize(1280,720)
-xout = pipeline.create(dai.node.XLinkOut)
-xout.setStreamName("rgb")
-cam.preview.link(xout.input)
-# Connect to device and start pipeline
-with dai.Device(pipeline) as device:
-    qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
-    print("UVC running")
-    while True:
-        frame = qRgb.get().getFrame()
-        
-        if frame is None:
-            print(' No captured frame -- Break!')
-            break
-        lane_image = np.copy(frame)
-        lane_canny = find_canny(lane_image,100,200)
-        # show_image('canny',lane_canny)
-        lane_roi = region_of_interest(lane_canny)
-        lane_lines = cv2.HoughLinesP(lane_roi,1,np.pi/180,50,40,5)
-        lane_lines_plotted = draw_lines(lane_image,lane_lines)
-        show_image('lines',lane_lines_plotted)
-        result_lines = compute_average_lines(lane_image,lane_lines)
-        print(result_lines)
-        final_lines_mask = draw_lines(lane_image,result_lines)
-        # show_image('final',final_lines_mask)
-        # for points in result_lines:
-        #     x1,y1,x2,y2 = points[0]
-        #     cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),2)
-        # show_image('output',frame)
+
+class LaneFollower:
+    def __init__(self):
+        self.stopped = False
+
+        # Create pipeline
+        self.pipeline = dai.Pipeline()
+        self.cam = self.pipeline.create(dai.node.ColorCamera)
+        # self.cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+        self.cam.setPreviewSize(640, 480)
+        self.cam.setInterleaved(False)
+
+        self.xout = self.pipeline.create(dai.node.XLinkOut)
+        self.xout.setStreamName("rgb")
+        self.cam.preview.link(self.xout.input)
+
+    def run(self):
+        # Connect to device and start pipeline
+        with dai.Device(self.pipeline) as device:
+            qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+            print("UVC running")
+            while not self.stopped:
+                frame = qRgb.get()
+
+                if frame is None:
+                    print(' No captured frame -- yikes!')
+                    continue
+                frame = cv2.cvtColor(frame.getCvFrame(), cv2.COLOR_BGR2RGB)
+
+                copy = np.copy(frame)
+                edges = canny(copy)
+                isolated = region(edges)
+                show_image('edges', edges)
+                show_image('isolated', isolated)
+
+                # DRAWING LINES: (order of params) --> region of interest, bin size (P, theta), min intersections needed, placeholder array,
+                lines = cv2.HoughLinesP(
+                    isolated, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+                if lines is None:
+                    continue
+                print(f"lines: {lines}, {lines.shape}")
+                averaged_lines = average(copy, lines)
+                print(
+                    f"averaged_lines: {averaged_lines}, {averaged_lines.shape}")
+                black_lines = display_lines(copy, averaged_lines)
+                # taking wighted sum of original image and lane lines image
+                lanes = cv2.addWeighted(copy, 0.8, black_lines, 1, 1)
+                print(lanes)
+                show_image('lanes', lanes)
+            device.close()
+
+    def stop(self, signal, frame):
+        print("gracefully stopping...")
+        self.stopped = True
+
+
+def main():
+    lf = LaneFollower()
+    signal.signal(signal.SIGTERM, lf.stop)
+    signal.signal(signal.SIGINT, lf.stop)
+    lf.run()
+
+
+if __name__ == "__main__":
+    main()
--- utils/lane_detect.py
+++ utils/lane_detect.py
@@ -1,106 +1,106 @@
-import cv2 as cv2 #importing the library
-import numpy as np
+import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-# image = cv2.imread('track_image.jpg') #Reading the image file
-# lane_image = np.copy(image)
-
-def show_image(name,img): #function for displaying the image
-    cv2.imshow(name,img)
+def show_image(name, img):  # function for displaying the image
+    cv2.imshow(name, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def find_canny(img,thresh_low,thresh_high): #function for implementing the canny
-    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+def grey(image):
+  # convert to grayscale
+    image = np.asarray(image)
+    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+  # Apply Gaussian Blur --> Reduce noise and smoothen image
+
+
+def gauss(image):
+    return cv2.GaussianBlur(image, (7, 7), 0)
+
+  # outline the strongest gradients in the image --> this is where lines in the image are
+
+
+def canny(image):
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # show_image('gray',img_gray)
-    img_blur = cv2.GaussianBlur(img_gray,(5,5),0)
+    img_blur = gauss(img_gray)
     # show_image('blur',img_blur)
-    img_canny = cv2.Canny(img_blur,thresh_low,thresh_high)
-    # show_image('Canny',img_canny)
-    return img_canny
-
-def region_of_interest(image): #function for extracting region of interest
-    #bounds in (x,y) format
-    bounds = np.array([[[0,250],[0,200],[150,100],[500,100],[650,200],[650,250]]],dtype=np.int32)
-    
-    # bounds = np.array([[[0,image.shape[0]],[0,image.shape[0]/2],[900,image.shape[0]/2],[900,image.shape[0]]]],dtype=np.int32)
-    mask=np.zeros_like(image)
-    cv2.fillPoly(mask,bounds,[255,255,255])
-    show_image('inputmask',mask)
-    masked_image = cv2.bitwise_and(image,mask)
-    show_image('mask',masked_image) 
-    return masked_image
+    edges = cv2.Canny(img_blur, 50, 150)
+    return edges
 
 
-def draw_lines(img,lines): #function for drawing lines on black mask
-    mask_lines=np.zeros_like(img)
-    for points in lines:
-        x1,y1,x2,y2 = points[0]
-        cv2.line(mask_lines,(x1,y1),(x2,y2),[0,0,255],2)
-
-    return mask_lines
-
-def get_coordinates(img,line_parameters): #functions for getting final coordinates
-    slope=line_parameters[0]
-    intercept = line_parameters[1]
-    y1 =300
-    y2 = 120
-    # y1=img.shape[0]
-    # y2 = 0.6*img.shape[0]
-    x1= int((y1-intercept)/slope)
-    x2 = int((y2-intercept)/slope)
-    return [x1,int(y1),x2,int(y2)]
-
-def compute_average_lines(img,lines):
-    left_lane_lines=[]
-    right_lane_lines=[]
-    left_weights=[]
-    right_weights=[]
-    for points in lines:
-        x1,y1,x2,y2 = points[0]
-        if x2==x1:
-            continue     
-        parameters = np.polyfit((x1,x2),(y1,y2),1) #implementing polyfit to identify slope and intercept
-        slope,intercept = parameters
-        length = np.sqrt((y2-y1)**2+(x2-x1)**2)
-        if slope <0:
-            left_lane_lines.append([slope,intercept])
-            left_weights.append(length)         
-        else:
-            right_lane_lines.append([slope,intercept])
-            right_weights.append(length)
-    # Computing average slope and intercept
-    left_average_line = np.average(left_lane_lines,axis=0)
-    right_average_line = np.average(right_lane_lines,axis=0)
-    print(left_average_line,right_average_line)
-    # #Computing weigthed sum
-    # if len(left_weights)>0:
-    #     left_average_line = np.dot(left_weights,left_lane_lines)/np.sum(left_weights)
-    # if len(right_weights)>0:
-    #     right_average_line = np.dot(right_weights,right_lane_lines)/np.sum(right_weights)
-    left_fit_points = get_coordinates(img,left_average_line)
-    right_fit_points = get_coordinates(img,right_average_line) 
-    # print(left_fit_points,right_fit_points)
-    return [[left_fit_points],[right_fit_points]] #returning the final coordinates
+def region(image):
+    height, width = image.shape
+    # isolate the gradients that correspond to the lane lines
+    rectangle = np.array([
+        [(0, height), (0, height//2), (width, height//2), (width, height)]
+    ])
+    # create a black image with the same dimensions as original image
+    mask = np.zeros_like(image)
+    # create a mask (triangle that isolates the region of interest in our image)
+    mask = cv2.fillPoly(mask, rectangle, 255)
+    mask = cv2.bitwise_and(image, mask)
+    return mask
 
 
-# ##Implementation
+def display_lines(image, lines):
+    lines_image = np.zeros_like(image)
+    # make sure array isn't empty
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line
+            # draw lines on a black image
+            cv2.line(lines_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return lines_image
 
-# lane_canny = find_canny(lane_image,100,200)
-# lane_roi = region_of_interest(lane_canny)
-# lane_lines = cv2.HoughLinesP(lane_roi,1,np.pi/180,50,40,5)
-# # print(lane_lines)
-# lane_lines_plotted = draw_lines(lane_image,lane_lines)
-# show_image('lines',lane_lines_plotted)
-# result_lines = compute_average_lines(lane_image,lane_lines)
-# print(result_lines)
-# final_lines_mask = draw_lines(lane_image,result_lines)
-# show_image('final',final_lines_mask)
 
-# for points in result_lines:
-#     x1,y1,x2,y2 = points[0]
-#     cv2.line(lane_image,(x1,y1),(x2,y2),(0,0,255),5)
+def average(image, lines):
+    left = []
+    right = []
 
-# show_image('output',lane_image)
+    width = image.shape[1]
+    height = image.shape[0]
 
+    if lines is not None:
+        for line in lines:
+            print(line)
+            x1, y1, x2, y2 = line.reshape(4)
+            # fit line to points, return slope and y-int
+            parameters = np.polyfit((x1, x2), (y1, y2), 1)
+            print(parameters)
+            slope = parameters[0]
+            y_int = parameters[1]
+            # lines on the right have positive slope, and lines on the left have neg slope
+            if slope < 0:
+                left.append((slope, y_int))
+            else:
+                right.append((slope, y_int))
+
+    left_line = [0, 0, 0, height]
+    right_line = [width, 0, width, height]
+    if len(left) != 0:
+        left_average = np.average(left, axis=0)
+        left_line = make_points(image, left_average)
+        print(f"valid left line {left_line}")
+    if len(right) != 0:
+        right_average = np.average(right, axis=0)
+        right_line = make_points(image, right_average)
+        print(f"valid right line {right_line}")
+
+    return np.array([left_line, right_line])
+
+
+def make_points(image, average):
+    print(f"average {average}")
+
+    slope, y_int = average
+    y1 = image.shape[0]
+    # how long we want our lines to be --> 3/5 the size of the image
+    y2 = int(y1 * (3/5))
+    # determine algebraically
+    x1 = int((y1 - y_int) // slope)
+    x2 = int((y2 - y_int) // slope)
+    return np.array([x1, y1, x2, y2])
